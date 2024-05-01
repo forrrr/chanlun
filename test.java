2abdb6d4618bf4e3d16d191d28f6af393fcd605c
mac air <463577231@qq.com>
2024-05-01 16:39:51 +0800

    f

---
+++ test.java
@@ -0,0 +1,10 @@
+
+public static void main(String[] args) {
+    int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int n = arr.length;
+    int k = 3;
+    System.out.println(" Array   after   " + k + "   rotations   to   the   right   is : ");
+    for (int i = 0; i < n; i++) {
+        System.out.print()
+    }
+}
